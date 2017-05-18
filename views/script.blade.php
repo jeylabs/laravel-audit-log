625dc0ae6e53cad80ad75664e0a76db446eace0e
Ratheep <ratheep.ceymplon@jeylabs.com>
2017-05-18 11:10:21 +0530

    error handiling update

--- views/script.blade.php
+++ views/script.blade.php
@@ -1,18 +1,44 @@
 <script>
     var auditLogVariables = {};
     auditLogVariables.prefix = '{{ route('audit.log.push.location') }}';
+    auditLogVariables.activity_log_id = null;
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(updateLocation);
+        navigator.geolocation.getCurrentPosition(updateLocation, showError, {
+            maximumAge: 60000,
+            timeout: 5000,
+            enableHighAccuracy: true
+        });
+    } else {
+        console.warn('Geolocation is not supported by this browser.')
     }
     function updateLocation(position) {
-        auditLogVariables.activity_log_id = '{{  activity('Visited')->causedBy(auth()->user())->log(url()->current())->id }}';
+        @if(config('laravel-audit-log.record_visiting'))
+            auditLogVariables.activity_log_id = '{{  activity('Visited')->causedBy(auth()->user())->log(url()->current())->id }}';
+                @endif
         var xHttp = new XMLHttpRequest();
         xHttp.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
             }
         };
-        auditLogVariables.url = auditLogVariables.prefix + '?audit_latitude='+position.coords.latitude+'&audit_longitude='+position.coords.longitude+'&audit_id='+auditLogVariables.activity_log_id;
+        auditLogVariables.url = auditLogVariables.prefix + '?audit_latitude=' + position.coords.latitude + '&audit_longitude=' + position.coords.longitude + '&audit_id=' + auditLogVariables.activity_log_id;
         xHttp.open("POST", auditLogVariables.url, true);
         xHttp.send();
+    }
+
+    function showError(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                console.warn("User denied the request for Geolocation.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                console.warn("Location information is unavailable.");
+                break;
+            case error.TIMEOUT:
+                console.warn("The request to get user location timed out.");
+                break;
+            case error.UNKNOWN_ERROR:
+                console.warn("An unknown error occurred.");
+                break;
+        }
     }
 </script>
