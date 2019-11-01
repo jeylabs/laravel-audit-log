2440f6c22d1259547611273ddb3c364e8eaf9595
majus28 <majushan97@gmail.com>
2019-11-01 08:40:21 +0530

    Update LogsAudit.php

--- src/Traits/LogsAudit.php
+++ src/Traits/LogsAudit.php
@@ -92,13 +92,13 @@ trait LogsAudit
             return true;
         }
 
-        if (array_has($this->getDirty(), 'deleted_at')) {
+        if (Arr::has($this->getDirty(), 'deleted_at')) {
             if ($this->getDirty()['deleted_at'] === null) {
                 return false;
             }
         }
 
         //do not log update event if only ignored attributes are changed
-        return (bool) count(array_except($this->getDirty(), $this->attributesToBeIgnored()));
+        return (bool) count(Arr::except($this->getDirty(), $this->attributesToBeIgnored()));
     }
 }
